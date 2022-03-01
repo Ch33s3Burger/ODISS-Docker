fb43800708d8d05f487262a99f657a4840f1c803
Benno <benno.dill@laulappen.de>
2022-03-01 15:22:42 +0100

    Added script to auto configure the Kafka ingestions in Druid

---
+++ auto_kafka_ingestion.sh
@@ -0,0 +1,8 @@
+curl -k -XPOST -H'Content-Type: application/json' -d @kafka_supervisor_config/form.json https://localhost/coordinator/druid/indexer/v1/supervisor
+curl -k -XPOST -H'Content-Type: application/json' -d @kafka_supervisor_config/item.json https://localhost/coordinator/druid/indexer/v1/supervisor
+curl -k -XPOST -H'Content-Type: application/json' -d @kafka_supervisor_config/itemgroup.json https://localhost/coordinator/druid/indexer/v1/supervisor
+curl -k -XPOST -H'Content-Type: application/json' -d @kafka_supervisor_config/study.json https://localhost/coordinator/druid/indexer/v1/supervisor
+curl -k -XPOST -H'Content-Type: application/json' -d @kafka_supervisor_config/studyevent.json https://localhost/coordinator/druid/indexer/v1/supervisor
+curl -k -XPOST -H'Content-Type: application/json' -d @kafka_supervisor_config/subject.json https://localhost/coordinator/druid/indexer/v1/supervisor
+curl -k -XPOST -H'Content-Type: application/json' -d @kafka_supervisor_config/subjectentry.json https://localhost/coordinator/druid/indexer/v1/supervisor
+curl -k -XPOST -H'Content-Type: application/json' -d @kafka_supervisor_config/user.json https://localhost/coordinator/druid/indexer/v1/supervisor
