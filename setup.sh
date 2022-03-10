05adfdc20654fc3fa1cf37168df28741c5ec14d1
Ch33s3Burger <benno.dill@laulappen.de>
2022-03-10 09:20:08 +0100

    created setup.sh script which uses the env/dev.properties to file all odiss settings into the templates.

---
+++ setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+ENV=${1:-dev}
+
+export $(grep -v '^#' ./env/${ENV}.properties | xargs)
+
+cp -R ./config_templates/. ./config
+
+envsubst < docker-compose.yml.template > docker-compose.yml
+envsubst < config_templates/druid/environment > config/druid/environment
+envsubst < config_templates/kafka/kafka_jaas.conf > config/kafka/kafka_jaas.conf
+envsubst < config_templates/kafka/server.properties > config/kafka/server.properties
+envsubst < config_templates/nginx/nginx.conf > config/nginx/nginx.conf
+envsubst < config_templates/trino/catalog/mpn.properties > config/trino/catalog/mpn.properties
+envsubst < config_templates/zookeeper/zookeeper_jaas.conf > config/zookeeper/zookeeper_jaas.conf
+
+envsubst < config_templates/druid/druid_supervisor_config/form.json > config/druid/druid_supervisor_config/form.json
+envsubst < config_templates/druid/druid_supervisor_config/item.json > config/druid/druid_supervisor_config/item.json
+envsubst < config_templates/druid/druid_supervisor_config/itemgroup.json > config/druid/druid_supervisor_config/itemgroup.json
+envsubst < config_templates/druid/druid_supervisor_config/study.json > config/druid/druid_supervisor_config/study.json
+envsubst < config_templates/druid/druid_supervisor_config/studyevent.json > config/druid/druid_supervisor_config/studyevent.json
+envsubst < config_templates/druid/druid_supervisor_config/subject.json > config/druid/druid_supervisor_config/subject.json
+envsubst < config_templates/druid/druid_supervisor_config/subjectentry.json > config/druid/druid_supervisor_config/subjectentry.json
+envsubst < config_templates/druid/druid_supervisor_config/user.json > config/druid/druid_supervisor_config/user.json
+
+envsubst < scripts/auto_kafka_ingestion.sh.template > scripts/auto_kafka_ingestion.sh
+
+touch ./config/trino/password.db
+htpasswd -b -B -C 10 ./config/trino/password.db ${ODISS_TRINO_USERNAME} ${ODISS_TRINO_PASSWORD}
