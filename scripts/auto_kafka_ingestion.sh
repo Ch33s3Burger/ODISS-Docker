0dd57901b92e2f61c23a3b68571009ffe4d4cf81
Ch33s3Burger <benno.dill@laulappen.de>
2022-03-10 10:17:42 +0100

    refactor: deleted script created over a template. And add them to .gitignore

--- scripts/auto_kafka_ingestion.sh
+++
@@ -1,8 +0,0 @@
-curl -u admin:password1 -k -XPOST -H'Content-Type: application/json' -d @../config/druid/druid_supervisor_config/form.json https://137.226.147.150/coordinator/druid/indexer/v1/supervisor
-curl -u admin:password1 -k -XPOST -H'Content-Type: application/json' -d @../config/druid/druid_supervisor_config/item.json https://137.226.147.150/coordinator/druid/indexer/v1/supervisor
-curl -u admin:password1 -k -XPOST -H'Content-Type: application/json' -d @../config/druid/druid_supervisor_config/itemgroup.json https://137.226.147.150/coordinator/druid/indexer/v1/supervisor
-curl -u admin:password1 -k -XPOST -H'Content-Type: application/json' -d @../config/druid/druid_supervisor_config/study.json https://137.226.147.150/coordinator/druid/indexer/v1/supervisor
-curl -u admin:password1 -k -XPOST -H'Content-Type: application/json' -d @../config/druid/druid_supervisor_config/studyevent.json https://137.226.147.150/coordinator/druid/indexer/v1/supervisor
-curl -u admin:password1 -k -XPOST -H'Content-Type: application/json' -d @../config/druid/druid_supervisor_config/subject.json https://137.226.147.150/coordinator/druid/indexer/v1/supervisor
-curl -u admin:password1 -k -XPOST -H'Content-Type: application/json' -d @../config/druid/druid_supervisor_config/subjectentry.json https://137.226.147.150/coordinator/druid/indexer/v1/supervisor
-curl -u admin:password1 -k -XPOST -H'Content-Type: application/json' -d @../config/druid/druid_supervisor_config/user.json https://137.226.147.150/coordinator/druid/indexer/v1/supervisor
